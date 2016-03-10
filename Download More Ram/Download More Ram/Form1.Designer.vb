1673066faaa2f93ad63f7fc510d8aa02391e23a8
x42f2039 <x42f2039@outlook.com>
2016-03-10 09:15:48 -0500

    adds more features

--- Download More Ram/Download More Ram/Form1.Designer.vb
+++ Download More Ram/Download More Ram/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Button1
@@ -44,15 +45,27 @@ Partial Class Form1
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Welcome To Download More Ram"
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(520, 337)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "End User License Agreement:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "By clicking the agree button below, you (the use" &
+    "r) agree to be legally bound to this" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "agreement, under penalty of death."
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(544, 412)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Download More Ram"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -60,4 +73,5 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- Download More Ram/Download More Ram/Form1.vb
+++ Download More Ram/Download More Ram/Form1.vb
@@ -2,5 +2,12 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Visible = False
         Button1.Visible = False
+        TextBox1.Visible = False
+        Me.Visible = False
+        Form2.ShowDialog()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
     End Sub
 End Class
